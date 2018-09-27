0ca97cde1f3a636b31f74ea80d816d001ec6aab2
user <user@DESKTOP-7EQF3M8>
2018-09-27 17:56:41 +0900

    re

--- WorldTour/src/city/Beijing.java
+++ WorldTour/src/city/Beijing.java
@@ -1,5 +1,23 @@
 package city;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Beijing extends City{
+
+	String name=null; // 필드 : 국가명, 크기, 유적지, 음식명 
+	int size; // 크기
+	List<String>historicsite = new ArrayList<>(); 
+	List<String>food = new ArrayList<>();
+	
+//	@Override
+//	public void print() {
+//		System.out.println("도시 이름 : " + this.name);
+//		System.out.println("도시 크기 : " + this.size);
+//		System.out.print("유적지 : ");
+//		for(String historicname : this.historicsite) {
+//			System.out.println(historicname);
+//		}
+//	}
 
 }
